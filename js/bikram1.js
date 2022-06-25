2825e4ac0787ad760a22fc7af41878df2fadfad8
Nacho-sf <serranof.ignacio@gmail.com>
2022-06-25 22:36:44 +0200

    bikram2: unsolved: arrays, functions

--- js/bikram1.js
+++ js/bikram1.js
@@ -1,22 +1,23 @@
 
-///////////// Pair Programming
+///////////// Pair Programming /////////////
 
 
 //Variables
  
-/*1.- Crear variable tipo let de nombre variableSinValor declarada sin valor*/
+
+//1.- Crear variable tipo let de nombre variableSinValor declarada sin valor
 
 
 
-/*2.- Crear 2 variables tipo let de nombres booleano1 y booleano2 con valores booleanos*/
+//2.- Crear 2 variables tipo let de nombres booleano1 y booleano2 con valores booleanos
 
 
 
-/*3.- Crear variable tipo const de nombre PI declarada con valor 3.14*/
+//3.- Crear variable tipo const de nombre PI declarada con valor 3.14
 
 
 
-/*4.- Crear variable tipo const de nombre TAU declarada con valor 2 veces PI*/
+//4.- Crear variable tipo const de nombre TAU declarada con valor 2 veces PI
 
 
 
@@ -25,15 +26,16 @@
 
 //Booleanos
 
-/*5.- Crear variable booleanoAnd cuyo valor sea la comparación booleana booleano1 and booleano2*/
+
+//5.- Crear variable booleanoAnd cuyo valor sea la comparación booleana booleano1 and booleano2
 
 
 
-/*6.- Crear variable booleanoNot cuyo valor sea la compracación booleana no booleano1*/
+//6.- Crear variable booleanoNot cuyo valor sea la compracación booleana no booleano1
 
 
 
-/*7.- Crear variable booleanoMix0 cuyo valor sea la compración booleana (booleano1 or booleano2) and (booleano1 or (not booleano1 and not booleano2)*/
+//7.- Crear variable booleanoMix0 cuyo valor sea la compración booleana (booleano1 or booleano2) and (booleano1 or (not booleano1 and not booleano2)
 
 
 
@@ -42,11 +44,12 @@
 
 //Operadores
 
-/*8.- Crear variable incrementarDesp con valor 2 y asigna su valor con postincremento a resultadoDesp*/
+
+//8.- Crear variable incrementarDesp con valor 2 y asigna su valor con postincremento a resultadoDesp
 
 
 
-/*9.- Crear variable incrementarAntes con valor 2 y asigna su valor con preincremento a resultadoAntes*/
+//9.- Crear variable incrementarAntes con valor 2 y asigna su valor con preincremento a resultadoAntes
 
 
 
@@ -55,36 +58,38 @@
 
 //Bucles
 
-/*10.- Crear variable contarHasta10_2 con valor 0 e incrementar su valor con un bucle for hasta que se verifique que contarHasta10_2 === 10*/
+
+//10.- Crear variable contarHasta10_2 con valor 0 e incrementar su valor con un bucle for hasta que se verifique que contarHasta10_2 === 10
 
 
 
-/*11.- Crear las variables postI y postJ con valor 0 a continuación cree un bucle que itere 11 veces. En cada iteración se deberá sumar al valor de postI el valor de postJ++*/
+//11.- Crear las variables postI y postJ con valor 0 a continuación cree un bucle que itere 11 veces. En cada iteración se deberá sumar al valor de postI el valor de postJ++
 
 
 
-/*12.- Crear la variable sumaPares con valor 0 a continuación crea un bucle que itere 10 veces (i < 10) si la iteración es par se deberá sumar a sumaPares el número de la iteración actual (i)*/
+//12.- Crear la variable sumaPares con valor 0 a continuación crea un bucle que itere 10 veces (i < 10) si la iteración es par se deberá sumar a sumaPares el número de la iteración actual (i)
 
 
 
 
 
 
-//////////// Proyecto individual
+//////////// Proyecto individual /////////////
 
 
 
 //Variables
 
-/*13.- Crear variable tipo let de nombre variableValorNumerico declarada con un valor numérico cualquiera*/
+
+//13.- Crear variable tipo let de nombre variableValorNumerico declarada con un valor numérico cualquiera
 
 
 
-/*14.- Crear variable tipo const de nombre MiNombre declarada con valor tu nombre*/
+//14.- Crear variable tipo const de nombre MiNombre declarada con valor tu nombre
 
 
 
-/*15.- Crear variable tipo const de nombre MiNumeroFav declarada con valor numérico*/
+//15.- Crear variable tipo const de nombre MiNumeroFav declarada con valor numérico
 
 
 
@@ -93,19 +98,20 @@
 
 //Booleanos
 
-/*16.- Crear variable booleanoOr cuyo calor sea la comparación booleana booleano1 or booleano2*/
+
+//16.- Crear variable booleanoOr cuyo calor sea la comparación booleana booleano1 or booleano2
 
 
 
-/*17.- Crear variable booleanoMix1 cuyo valor sea la comparación booleana (booleano1 and (TAU/2 sea igual a PI)) or (variableValorNumerico mayor o igual que MiNumeroFav)*/
+//17.- Crear variable booleanoMix1 cuyo valor sea la comparación booleana (booleano1 and (TAU/2 sea igual a PI)) or (variableValorNumerico mayor o igual que MiNumeroFav)
 
 
 
-/*18.- Crear variable seisNoEsNueve cuyo valor sea la comparación booleana 6 no es estrictamente igual que 9*/
+//18.- Crear variable seisNoEsNueve cuyo valor sea la comparación booleana 6 no es estrictamente igual que 9
 
 
 
-/*19.- Crear variable booleanoMix2 cuyo valor sea la comparación booleana variableValorNumerico positivo (0 no incluido) o menor que -(MiNumeroFav * TAU)*/
+//19.- Crear variable booleanoMix2 cuyo valor sea la comparación booleana variableValorNumerico positivo (0 no incluido) o menor que -(MiNumeroFav * TAU)
 
 
 
@@ -114,19 +120,20 @@
 
 //Operadores
 
-/*20.- Crear variable valorSuma cuyo valor sea la suma de MiNumeroFav y variableValorNumerico*/
+
+//20.- Crear variable valorSuma cuyo valor sea la suma de MiNumeroFav y variableValorNumerico
 
 
 
-/*21.- Crear variable valorResta cuyo valor sea la resta de MiNumeroFav y variableValorNumerico*/
+//21.- Crear variable valorResta cuyo valor sea la resta de MiNumeroFav y variableValorNumerico
 
 
 
-/*22.- Crear variable valorMultiplicación cuyo valor sea la multiplicación de MiNumeroFav por variableValorNumerico*/
+//22.- Crear variable valorMultiplicación cuyo valor sea la multiplicación de MiNumeroFav por variableValorNumerico
 
 
 
-/*23.- Crear variable valorDivisión cuyo valor sea la división de MiNumeroFav entre 3*/
+//23.- Crear variable valorDivisión cuyo valor sea la división de MiNumeroFav entre 3
 
 
 
@@ -135,12 +142,13 @@
 
 //Bucles
 
-/*24.- Crear variable contarHasta10 con valor 0 e incrementar su valor con un bucle while hasta que se verifique que contarHasta10 === 10*/
+
+//24.- Crear variable contarHasta10 con valor 0 e incrementar su valor con un bucle while hasta que se verifique que contarHasta10 === 10
 
 
 
-/*25.- Crear las variables preI y preJ con valor 0 a continuación cree un bucle que itere 11 veces. En cada iteración se deberá sumar al valor de preI el valor de ++preJ*/
+//25.- Crear las variables preI y preJ con valor 0 a continuación cree un bucle que itere 11 veces. En cada iteración se deberá sumar al valor de preI el valor de ++preJ
 
 
 
-/*26.- Crear la variable sumaImpares con valor 0 a continuación crea un bucle que itere 10 veces (i < 10) si la iteración es impar se deberá sumar a sumaImpares el número de la iteración actual (i)*/
+//26.- Crear la variable sumaImpares con valor 0 a continuación crea un bucle que itere 10 veces (i < 10) si la iteración es impar se deberá sumar a sumaImpares el número de la iteración actual (i)
